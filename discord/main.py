015d19f9c18b26e834ac4bc0b315fb5c6f7722d1
Varadskajgar <130847658+Varadskajgar@users.noreply.github.com>
2025-11-03 07:55:02 +0530

    Create main.py

---
+++ discord/main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="?", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    latency = round(bot.latency * 1000)
+    await ctx.send(f"Pong! 🏓 {latency}ms")
+
+TOKEN = os.getenv("TOKEN")
+
+bot.run(TOKEN)
